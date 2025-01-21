5e688a0d27b8fb2a318edabe09cdb34b575eaf60
Camilo Cortes <100862481+camilocortes27@users.noreply.github.com>
2025-01-21 14:38:02 -0500

    proyecto terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
-const Home = () => {
+const semaforo = () => {
+	const [luzVerde, setLuzVerde] = useState("green-light");
+	const [luzAmarilla, setLuzAmarilla] = useState("yellow-light");
+	const [luzRoja, setLuzRoja] = useState("red-light");
+	const [stylesDefaults, setStylesDefaults] = useState(["green-light", "yellow-light", "red-light"]);
+
+	const handleClick = (e) => {
+		if (e.target.classList == stylesDefaults[0]) {
+			setLuzVerde("green-light on");
+			setLuzAmarilla(stylesDefaults[1]);
+			setLuzRoja(stylesDefaults[2]);
+		} else {
+			setLuzVerde(stylesDefaults[0]);
+		}
+		if (e.target.classList == stylesDefaults[1]) {
+			setLuzVerde(stylesDefaults[0]);
+			setLuzAmarilla("yellow-light on");
+			setLuzRoja(stylesDefaults[2]);
+		} else {
+			setLuzAmarilla(stylesDefaults[1]);
+		}
+		if (e.target.classList == stylesDefaults[2]) {
+			setLuzVerde(stylesDefaults[0]);
+			setLuzAmarilla(stylesDefaults[1]);
+			setLuzRoja("red-light on");
+		} else {
+			setLuzRoja(stylesDefaults[2]);
+		}
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container-fluid top"></div>
+			<div className="container d-flex justify-content-center">
+				<div className="box d-flex flex-column align-items-center justify-content-center gap-1">
+					<div className={luzVerde} onClick={handleClick}></div>
+					<div className={luzAmarilla} onClick={handleClick}></div>
+					<div className={luzRoja} onClick={handleClick}></div>
+				</div>
+			</div>
+		</>
 	);
 };
 
-export default Home;
+export default semaforo;
